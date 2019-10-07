948c7562d58c05132a10df597ecffe1dccc51983
yezki8 <yehezkiel.live@gmail.com>
2019-10-07 17:13:29 +0700

    Dodng sudah dapat di spawn

--- Assets/Script/AddBus.cs
+++ Assets/Script/AddBus.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AddBus : MonoBehaviour {
 
     MoveOnPath odong;
-    public GameObject OriOdong;
+    public GameObject IpaOdong;
+    public GameObject IpsOdong;
     public GameObject ParOdong;
     public GameObject SpawnerLocation;
     GameObject CloneOdong;
@@ -15,14 +16,29 @@ public class AddBus : MonoBehaviour {
     int point;
     int nomor = 1;
 
-	public void addClone()
+	public void addCloneIpa()
     {
         nomor += 1;
-        CloneOdong = Instantiate(OriOdong, transform.position, transform.rotation) as GameObject;
+        CloneOdong = Instantiate(IpaOdong, transform.position, transform.rotation) as GameObject;
         CloneOdong.transform.parent = ParOdong.transform;
-        CloneOdong.transform.position = new Vector3(29.86314f, 1.4603f, 0);
-        CloneOdong.transform.localScale = new Vector3(1, 1, 1);
-        CloneOdong.name = "bus_" + nomor.ToString();
+        CloneOdong.transform.position = SpawnerLocation.transform.position;
+        CloneOdong.transform.localScale = IpaOdong.transform.localScale;
+        CloneOdong.name = "Bus_" + nomor.ToString();
+
+        //SetClone = GameObject.Find("ipa_" + nomor.ToString());
+        //SetClone.GetComponent < currentWayPointID > = 0;
+        //point = 0;
+        //this.odong.currentWayPointID = point;
+    }
+
+    public void addCloneIps()
+    {
+        nomor += 1;
+        CloneOdong = Instantiate(IpsOdong, transform.position, transform.rotation) as GameObject;
+        CloneOdong.transform.parent = ParOdong.transform;
+        CloneOdong.transform.position = SpawnerLocation.transform.position;
+        CloneOdong.transform.localScale = IpsOdong.transform.localScale;
+        CloneOdong.name = "Bus_" + nomor.ToString();
 
         //SetClone = GameObject.Find("ipa_" + nomor.ToString());
         //SetClone.GetComponent < currentWayPointID > = 0;
